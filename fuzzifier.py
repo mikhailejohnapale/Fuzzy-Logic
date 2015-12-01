2b4c06eefca754a45838ba0bddee3a537acb8e0d
Rodel Rebucas <rodelrebucas@gmail.com>
2015-12-01 08:04:08 +0800

    testing done

--- fuzzifier.py
+++ fuzzifier.py
@@ -1,22 +1,17 @@
 class Fuzzifier:
-    test = "test 2"
+    label = []
+    crisp_input = []
+    fuzzy_val = []
 
     def __init__(self, input_, label):
         self.input_ = input_
         self.label = label
-        #self.test = "this is a test"
 
     def getFuzzy(self):
-        self.test = "test 3"
-        # return self.list
+        pass
 
     def detSubset(self):
         pass
 
     def getTriMf(self):
         pass
-
-a = Fuzzifier(1, 2)
-print(a.test)
-a.getFuzzy()
-print(a.test)
