0a08e268992fe5715369289766c90f29db9cbc35
Rodel Rebucas <rodelrebucas@gmail.com>
2015-12-09 09:02:04 +0800

    initial getFuzzy

--- fuzzifier.py
+++ fuzzifier.py
@@ -1,6 +1,5 @@
 class Fuzzifier:
     label = []
-    crisp_input = []
     fuzzy_val = []
 
     def __init__(self, input_, label):
@@ -8,10 +7,28 @@ class Fuzzifier:
         self.label = label
 
     def getFuzzy(self):
-        pass
+        # call detSubset and geTriMf and store it in fuzzy_val
+        for i in self.label:
+            if self.crisp_input in i:
+                print(i)
 
     def detSubset(self):
+        # scan  the fuzzy set and
         pass
 
     def getTriMf(self):
+        # formula triMf
+        #(x = input,a,b,c) feet and peak
+        #max(min((x - a) / (b - a), (c - x) / (c - b)), 0)
+
         pass
+
+
+velocitySet = [
+    [
+        "very slow", 0, 0.4], [
+        "slow", 0.2, 0.6], [
+        "moderate", 0.4, 0.8], [
+        "fast", 0.6, 1]]
+
+Fuzzifier(0.4, velocitySet).getFuzzy()
