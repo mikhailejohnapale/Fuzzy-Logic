535b8fc1b7ebd6f17bafc46f5aa0144d5ecac193
Rodel Rebucas <rodelrebucas@gmail.com>
2015-12-14 08:22:54 +0800

    set trafficCongestion peak value

--- trafficCongestion.py
+++ trafficCongestion.py
@@ -19,23 +19,23 @@ class TrafficCongestion:
     ]
     velocitySet = [
         [
-            "very slow", 0, 0.4], [
-            "slow", 0.2, 0.6], [
-            "moderate", 0.4, 0.8], [
-            "fast", 0.6, 1]]
+            "very slow", 0, 0.2, 0.4], [
+            "slow", 0.2, 0.4, 0.6], [
+            "moderate", 0.4, 0.6, 0.8], [
+            "fast", 0.6, 0.8, 1]]
     densitySet = [
         [
-            "low", 0, 0.4], [
-            "moderate", 0.2, 0.6], [
-            "high", 0.4, 0.8], [
-            "very high", 0.6, 1]]
+            "low", 0, 0.2, 0.4], [
+            "moderate", 0.2, 0.4, 0.6], [
+            "high", 0.4, 0.6, 0.8], [
+            "very high", 0.6, 0.8, 1]]
 
     congestionSet = [
         [
-            "free", 0, 0.4], [
-            "low", 0.2, 0.6], [
-            "moderate", 0.4, 0.8], [
-            "high", 0.6, 1]]
+            "free", 0, 0.2, 0.4], [
+            "low", 0.2, 0.4, 0.6], [
+            "moderate", 0.4, 0.6, 0.8], [
+            "high", 0.6, 0.8, 1]]
 
     def __init__(self):
         pass
