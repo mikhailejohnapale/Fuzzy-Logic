c4899415c0ec29a8ba16070a5426fa7367cb6aed
Rodel Rebucas <rodelrebucas@gmail.com>
2015-12-14 19:57:33 +0800

    working fuzzy value

--- fuzzylogic.py
+++ fuzzylogic.py
@@ -1,9 +1,21 @@
+from fuzzifier import *
+from trafficCongestion import *
+
+
 class FuzzyLogic:
-    input_ = []
-    output = []
 
     def __init__(self, input_):
         self.input_ = input_
 
     def getResult(self):
-        return self.output
+        self.fuzzyVal = [[], []]
+        self.fuzzyVal[0] = Fuzzifier(self.input_[0],
+                                     TrafficCongestion().getDensitySet()
+                                     ).getFuzzy()
+        self.fuzzyVal[1] = Fuzzifier(self.input_[1],
+                                     TrafficCongestion().getVelocitySet()
+                                     ).getFuzzy()
+        return self.fuzzyVal
+
+
+print(FuzzyLogic([0.3, 0.6]).getResult())
