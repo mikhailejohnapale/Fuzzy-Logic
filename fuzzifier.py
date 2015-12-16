e1ce537624baf0617bfa90da888f46e3c30ece91
Rodel Rebucas <rodelrebucas@gmail.com>
2015-12-16 21:04:54 +0800

    add trapezoid formula

--- fuzzifier.py
+++ fuzzifier.py
@@ -20,23 +20,30 @@ class Fuzzifier:
     def getTriMf(self):
         # (x = input,a,b,c) feet and peak
         # max(min((x - a) / (b - a), (c - x) / (c - b)), 0)
+        # max(min(((x - a) / (b - a)), 1, ((d - x) / (d - c))), 0)
         self.fuzzy_val = {}
         for a, b in self.subset.items():
             value = max(min(((self.crisp_input - b[0]) / (b[1] - b[0])),
                             ((b[2] - self.crisp_input) / (b[2] - b[1]))), 0)
             self.fuzzy_val.update({a: round(value, 2)})
 
-# velocitySet = {
-#     "very slow": [0, 0.2, 0.4],
-#     "slow": [0.2, 0.4, 0.6],
-#     "moderate": [0.4, 0.6, 0.8],
-#     "fast": [0.6, 0.8, 1]}
+    def Test(self):
+        return self.fuzzy_val, self.subset
+velocitySet = {
+    "very slow": [0, 0.2, 0.4],
+    "slow": [0.2, 0.4, 0.6],
+    "moderate": [0.4, 0.6, 0.8],
+    "fast": [0.6, 0.8, 1]}
 
-# densitySet = {
-#     "low": [0, 0.2, 0.4],
-#     "moderate": [0.2, 0.4, 0.6],
-#     "high": [0.4, 0.6, 0.8],
-#     "very high": [0.6, 0.8, 1]}
+densitySet = {
+    "low": [0, 0.2, 0.4],
+    "moderate": [0.2, 0.4, 0.6],
+    "high": [0.4, 0.6, 0.8],
+    "very high": [0.6, 0.8, 1]}
 
-# Fuzzifier(0.6, velocitySet).getFuzzy()
-# Fuzzifier(0.3, densitySet).getFuzzy()
+v = Fuzzifier(0, velocitySet)
+d = Fuzzifier(0, densitySet)
+v.getFuzzy()
+d.getFuzzy()
+print(v.Test())
+print(d.Test())
